a3be4e6bba425e498299c47e06e3ab96cc0f3c78
Missy Bridgwater <missy789@g.ucla.edu>
2024-12-10 17:29:55 -0800

    Support memoization and one version with cycles and one without

--- graphs/dfs/available_members_dfs.py
+++ graphs/dfs/available_members_dfs.py
@@ -21,28 +21,41 @@ Output: ["Thunders","Warriors","Rebooters"]
 """
 
 
-def full_availability(TeamsToSupporterMap, FolksAvailable):
+def full_availability_cycles(TeamsToSupporterMap, FolksAvailable):
     res = []
 
+    cache = {}
+
     def dfs(team, seen):
-        # root = team
-        # if team in seen:
-        #     return False  # cycle detected
-        # seen.add(team)
+        """This dfs algorithm accounts for caching and cycles"""
+        if team in seen and team not in cache:
+            print("cycle detected")
+            return False  # cycle detected
+        if team in cache:
+            return cache[team]
+
+        seen.add(team)
 
         for supporter in TeamsToSupporterMap.get(team, []):
             if supporter in TeamsToSupporterMap:
                 # this support is a team, so recursively call dfs
-                if not dfs(supporter, TeamsToSupporterMap):  # seen.add(supporter)
+                if not dfs(supporter, seen):
+                    cache[team] = False
                     return False
             else:
                 if supporter not in FolksAvailable:
+                    cache[team] = False
                     return False
-        # seen = set()
+        seen = set()
+        cache[team] = True
         return True
-        # return False
 
     for team in TeamsToSupporterMap:
+        """
+        In total, this is an O(N) algorithm, where N is the number of teams and we assume each team has some constant number of supporters.
+        Although it appears we are calling dfs N times (where dfs is an O(N) algorithm), we only build the graph once and use memoization so
+        that in reality, dfs is only run once through the whole graph.
+        """
         seen = set()
         if dfs(team, seen):
             res.append(team)
@@ -50,11 +63,56 @@ def full_availability(TeamsToSupporterMap, FolksAvailable):
     return res
 
 
+def full_availability_no_cycles(TeamsToSupporterMap, FolksAvailable):
+    res = []
+
+    cache = {}
+
+    def dfs(team):
+        """This dfs algorithm accounts for caching, but not for cycles"""
+        if team in cache:
+            return cache[team]
+
+        for supporter in TeamsToSupporterMap.get(team, []):
+            if supporter in TeamsToSupporterMap:
+                # this support is a team, so recursively call dfs
+                if not dfs(supporter):
+                    cache[team] = False
+                    return False
+            else:
+                if supporter not in FolksAvailable:
+                    cache[team] = False
+                    return False
+        cache[team] = True
+        return True
+
+    for team in TeamsToSupporterMap:
+        """
+        In total, this is an O(N) algorithm, where N is the number of teams and we assume each team has some constant number of supporters.
+        Although it appears we are calling dfs N times (where dfs is an O(N) algorithm), we only build the graph once and use memoization so
+        that in reality, dfs is only run once through the whole graph.
+        """
+        if dfs(team):
+            res.append(team)
+
+    return res
+
+
 if __name__ == "__main__":
-    TeamsToSupporterMap = {
+    TeamsToSupporterMap1 = {
         "Thunders": ["David", "Catherine"],
         "Warriors": ["Rebooters", "Sam", "Thunders"],
         "Rebooters": ["Thunders", "Sam"],
     }
-    FolksAvailable = ["Catherine", "David", "Sam"]
-    print(full_availability(TeamsToSupporterMap, FolksAvailable))
+    FolksAvailable1 = ["Catherine", "David", "Sam"]
+
+    TeamsToSupporterMap2 = {
+        "Thunders": ["David", "Warriors"],
+        "Warriors": ["Sam", "Thunders"],
+        "Rebooters": ["Thunders", "Sam"],
+    }
+    FolksAvailable2 = ["Catherine", "David", "Sam"]
+    print(full_availability_cycles(TeamsToSupporterMap1, FolksAvailable1))
+    print(full_availability_cycles(TeamsToSupporterMap2, FolksAvailable2))
+    print(full_availability_no_cycles(TeamsToSupporterMap1, FolksAvailable1))
+    # print(full_availability_no_cycles(TeamsToSupporterMap2, FolksAvailable2)) # throws error due to cycles
